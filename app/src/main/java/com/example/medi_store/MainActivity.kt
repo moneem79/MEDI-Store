5a31edcb652b088ed1c55f0862b5f991ed41c238
moneem79 <moneemmonower79@gmail.com>
2025-06-08 14:35:53 +0600

    Create UI and SIGN UP in Screens

--- app/src/main/java/com/example/medi_store/MainActivity.kt
+++ app/src/main/java/com/example/medi_store/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,14 +13,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.medi_store.ui.theme.MEDIStoreTheme
+import com.example.medi_store.ui.theme.Screens.Signup
+import com.example.medi_store.viewModel.MYViewModel
 
-class MainActivity : ComponentActivity() {
+ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val viewModel= viewModels<MYViewModel>()
             MEDIStoreTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Signup(viewModel.value)
 
                 }
             }
--- app/src/main/java/com/example/medi_store/ui/theme/Screens/Signup.kt
+++ app/src/main/java/com/example/medi_store/ui/theme/Screens/Signup.kt
@@ -1,4 +1,79 @@
 package com.example.medi_store.ui.theme.Screens
 
-class Signup {
-}
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.medi_store.viewModel.MYViewModel
+
+@Composable
+fun Signup(viewModel: MYViewModel) {
+
+    val name = remember { mutableStateOf("") }
+    val password = remember { mutableStateOf("") }
+    val phoneNumber = remember { mutableStateOf("") }
+    val email = remember { mutableStateOf("") }
+    val pinCode = remember { mutableStateOf("") }
+    val address = remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        TextField(
+            value = name.value,
+            onValueChange = { name.value = it },
+            label = { androidx.compose.material3.Text("Name") }
+        )
+
+        TextField(
+            value = password.value,
+            onValueChange = { password.value = it },
+            label = { androidx.compose.material3.Text("Password") }
+        )
+
+        TextField(
+            value = phoneNumber.value,
+            onValueChange = { phoneNumber.value = it },
+            label = { androidx.compose.material3.Text("Phone Number") }
+        )
+
+        TextField(
+            value = email.value,
+            onValueChange = { email.value = it },
+            label = { androidx.compose.material3.Text("Email") }
+        )
+
+        TextField(
+            value = pinCode.value,
+            onValueChange = { pinCode.value = it },
+            label = { androidx.compose.material3.Text("Pin Code") }
+        )
+
+        TextField(
+            value = address.value,
+            onValueChange = { address.value = it },
+            label = { androidx.compose.material3.Text("Address") }
+        )
+
+                Button(onClick = {
+                    viewModel.createUser(
+                        name = name.value,
+                        password = password.value,
+                        phoneNumber = phoneNumber.value,
+                        email = email.value,
+                        pinCode = pinCode.value,
+                        address = address.value)
+                }) {
+                    Text(text = "Sign Up")
+                }
+
+
+    }}
+
+
+
