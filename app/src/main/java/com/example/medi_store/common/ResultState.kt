f6caee44c2be511b460413a5ff98eab609c5dc1b
moneem79 <moneemmonower79@gmail.com>
2025-06-08 17:35:03 +0600

    Create Sealed Class

--- app/src/main/java/com/example/medi_store/common/ResultState.kt
+++ app/src/main/java/com/example/medi_store/common/ResultState.kt
@@ -1,6 +1,8 @@
 package com.example.medi_store.common
 
-sealed class ResultState{
+sealed class ResultState<T>{
 
-    data class Success(val data:T):ResultState<T>()
+    data class Success<T>(val data:T):ResultState<T>()
+    data class Error<T>(val exception: Exception):ResultState<T>()
+    object Loading:ResultState<Nothing>()
 }
