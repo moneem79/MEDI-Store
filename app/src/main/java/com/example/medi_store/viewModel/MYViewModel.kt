759bc352af10573729a8914d0111d700c94f96c2
moneem79 <moneemmonower79@gmail.com>
2025-06-08 14:05:23 +0600

    Create MYViewModel and use Corutine in this

--- app/src/main/java/com/example/medi_store/viewModel/MYViewModel.kt
+++ app/src/main/java/com/example/medi_store/viewModel/MYViewModel.kt
@@ -1,8 +1,33 @@
 package com.example.medi_store.viewModel
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.medi_store.Repo.Repo
+import com.example.medi_store.network.response.CreateUSerResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 class MYViewModel:ViewModel() {
 
-    val
+    val repo = Repo()
+    val createUserState= mutableStateOf<CreateUSerResponse?>(null)
+
+
+    fun createUser(
+        name:String,
+        password: String,
+        phoneNumber: String,
+        email: String,
+        pinCode: String,
+        address: String,
+    ){
+         viewModelScope.launch (Dispatchers.IO){
+             val response =repo.createUser(name,password,email,phoneNumber,pinCode, address)
+             createUserState.value=response.body()
+
+         }
+
+    }
 }
